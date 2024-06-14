6faa8959448504eaf97c99095d931c7960aafcc7
ViktoriyaPavlova <ophtalma@yandex.ru>
2024-06-14 19:52:50 +0300

    Создала Stepper

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,71 @@
 import "./App.css";
 import { Card } from "./components/ui/Card/Card";
-
-// Данные карточек (продукты)
-const initialProducts = [
-  {
-    id: "1",
-    title: "POLAROID",
-    category: " PLD 4139/S C3 09Q",
-    description:
-      "Солнцезащитные очки",
-    rating: "4.9",
-    price: "6390 \u20bd",
-    imgSrc: "../assets/products/pld-1.jpg",
-  },
-  {
-    id: "2",
-    title: "POLAROID",
-    category: "PLD 4141/G/S/X WJ 010",
-    description:
-      "Солнцезащитные очки",
-    rating: "3.0",
-    price: "7350 \u20bd",
-    imgSrc: "../assets/products/pld-2.jpg",
-  },
-  {
-    id: "3",
-    title: "POLAROID",
-    category: "PLD 8052/S JQ QCK (7-10 лет)",
-    description:
-      "Солнцезащитные очки",
-    rating: "4.9",
-    price: "5290 \u20bd",
-    imgSrc: "../assets/products/pld-3.jpg",
-  },
-  {
-    id: "4",
-    title: "POLAROID",
-    category: "PLD 8053/S M9 80Z (7-10 лет)",
-    description:
-      "Солнцезащитные очки",
-    rating: "3.0",
-    price: "4850 \u20bd",
-    imgSrc: "../assets/products/pld-4.jpg",
-  },
-  {
-    id: "5",
-    title: "POLAROID",
-    category: "PLD K006/S M9 C9A (4-6 лет)",
-    description:
-      "Солнцезащитные очки",
-    rating: "3.2",
-    price: "4850 \u20bd",
-    imgSrc: "../assets/products/pld-5.jpg",
-  },
-  {
-    id: "6",
-    title: "POLAROID",
-    category: "PLD K007/S M9 1ED (4-6 лет)",
-    description:
-      "Солнцезащитные очки",
-    rating: "1.0",
-    price: "4850 \u20bd",
-    imgSrc: "../assets/products/pld-6.jpg",
-  },
-  {
-    id: "7",
-    title: "POLAROID",
-    category: "PLD 8056/S M9 FLL",
-    description:
-      "Солнцезащитные очки",
-    rating: "1.0",
-    price: "5290 \u20bd",
-    imgSrc: "../assets/products/pld-7.jpg",
-  },
-  {
-    id: "8",
-    title: "POLAROID",
-    category: "PLD 6209/S/X HE 733",
-    description:
-      "Солнцезащитные очки",
-    rating: "1.0",
-    price: "7890 \u20bd",
-    imgSrc: "../assets/products/pld-8.jpg",
-  },
-];
+import { initialProducts } from "../data";
+import { useState } from "react";
 
 function App() {
+  // Стейт для продуктов (начальное значение из data.js)
+  const [products, setProducts] = useState(initialProducts);
+
+  // Обработчик клика по карточке (для открытия сайдбара, например)
+  const handleCardBtnClick = (id) => {
+    console.log(`Нажата карточка с ID ${id}`);
+  };
+
+  /**
+   * Обработка изменений в Stepper
+   * @param {string} id - id товара
+   * @param {string} newValue - Новое значение из Stepper
+   */
+  const handleStepperUpdate = (id, newValue) => {
+    const updatedProducts = initialProducts?.map((product) => {
+      if (product?.id === id) {
+        return { ...product, cartQuantity: newValue };
+      }
+
+      return product;
+    });
+
+    // Обновляем стейт
+    setProducts(updatedProducts);
+  };
+
+  /**
+   * Обработка добавления товара в избранное
+   * @param {string} id - id товара
+   */
+  const handleToggleFavorite = (id) => {
+    // Создании поверхностной копии массива
+    const currentProducts = [...products];
+
+    // Находим товара по id
+    const product = currentProducts?.find((product) => product?.id === id);
+
+    if (product) {
+      // Обновляем значение isFavorite у найденного товара
+      product.isFavorite = !product?.isFavorite;
+
+      // Обновляем стейт
+      setProducts(currentProducts);
+
+      // Получаем список избранных товаров
+      const favoriteProducts = currentProducts
+        ?.filter((product) => product?.isFavorite)
+        ?.map((product) => product?.id);
+
+      // Записываем избранные товары в localStorage
+      localStorage.setItem("favorite", JSON.stringify(favoriteProducts));
+    }
+
+    console.log("сохраненки", products);
+  };
+
   return (
     <section className="products py-10 bg-gray-100">
       <div className="container mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {!!initialProducts &&
           initialProducts.map((product) => (
-            <Card key={product?.id} details={product} />
+            <Card key={product?.id} details={product} onBtnClick={handleCardBtnClick} onStepperUpdate={handleStepperUpdate} onToggleFavorite={handleToggleFavorite} />
           ))}
       </div>
     </section>
