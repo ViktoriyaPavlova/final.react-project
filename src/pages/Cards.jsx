cbdc92bc9be9ccd24148065e1225df154f5355d7
ViktoriyaPavlova <ophtalma@yandex.ru>
2024-07-02 21:00:39 +0300

    Внесла изменения в работу стора.

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,19 +1,53 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components/ui/Card/Card";
 import useProductsStore from "../store/useProductsStore";
 import { useNavigate } from "react-router-dom";
+import Alert from "../components/ui/Alert/Alert";
 
 const Cards = () => {
   const navigate = useNavigate(); // хук для роутинга
 
   // Стор для работы с продуктами
-  const { products, setFavorite } = useProductsStore();
+  const { products, getProducts, onToggleFavorite, getProductById } =
+    useProductsStore();
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
 
   // Обработчик клика по карточке
   const handleCardClick = (id) => {
     navigate(`/cards/${id}`);
   };
 
+  // Стейт для скрытия/показа и передачи сообщения в Alert
+  const [alertState, setAlertState] = useState({
+    isOpen: false,
+    message: "",
+  });
+
+  // // Обработчик закрытия компонента Alert
+  const handleCloseAlert = () => {
+    setAlertState({ ...alertState, isOpen: false });
+  };
+
+  // // Обработчик добавления товара в сохраненки и показа уведомления
+  const handleFavoriteAndShowAlert = (id) => {
+    // Достаем из стора поле isFavorite выбранного продукта
+    const { isFavorite } = getProductById(id);
+
+    onToggleFavorite(id); // вкл/выкл товара в сохраненки
+
+    setAlertState({
+      isOpen: true,
+      message: isFavorite
+        ? "Товар удален из сохраненок"
+        : "Товар добавлен в сохраненки",
+    });
+  };
+
   return (
+    <>
     <section className="products py-10 bg-gray-100">
       <div className="container mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {!!products &&
@@ -22,11 +56,19 @@ const Cards = () => {
               key={product?.id}
               details={product}
               onCardClick={handleCardClick}
-              onToggleFavorite={setFavorite}
+              onHeartClick={handleFavoriteAndShowAlert}
             />
           ))}
       </div>
     </section>
+    <Alert
+    title="Сохранение товара."
+    subtitle={alertState?.message}
+    variant="neutral"
+    isOpen={alertState?.isOpen}
+    onClose={handleCloseAlert}
+  />
+</>
   );
 };
 
--- src/store/useProductsStore.js
+++ src/store/useProductsStore.js
@@ -1,33 +1,58 @@
 import { create } from "zustand";
-import { initialProducts } from "../../data.js";
+// import { initialProducts } from "../../data";
 
 /**
  * Стор для управления продуктами и состоянием сохраненных продуктов.
  */
 const useProductsStore = create((set) => {
+  // Инициализация переменной для хранения продуктов
+  let products;
+
   // Загрузка избранных продуктов из localStorage.
   const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
+  async function getProducts() {
+    try {
+      // Выполнение запроса
+      const response = await fetch("http://localhost:3000/products");
+
+      if (!response?.ok) {
+        throw new Error("Failed to fetch products");
+      }
+
+      // Асинхронная сериализация
+      const data = await response?.json();
+
+      // Перезапись переменной на полученные данные
+      products = data?.map((product) => ({
+        ...product,
+        isFavorite: storedFavorites?.includes(product?.id),
+      }));
+
+      set({ products });
+    } catch (error) {
+      console.error("Error fetching products");
+    }
+  }
+
   // Инициализация продуктов с учетом сохраненных состояний
-  const products = initialProducts?.map((product) => ({
-    ...product,
-    isFavorite: storedFavorites?.includes(product?.id),
-  }));
+  // const products = initialProducts?.map((product) => ({
+  //   ...product,
+  //   isFavorite: storedFavorites?.includes(product?.id),
+  // }));
 
   /**
-   * Находит продукт по id.
-   * @param {string} id - id продукта.
-   * @returns {Object|Null} Возвращает найденный продукт или null.
-   */
-  const getProductById = (id) =>
-    products?.find((product) => product?.id === id) || null;
+    Находит продукт по id.
+    @param {string} id - id продукта.
+    @returns {Object|null} Возвращает найденный продукт или null.
+    */
+  const getProductById = id => products?.find(product => product?.id === id) || null;
 
   /**
    * Переключает состояние сохраненного продукта по id.
    * @param {string} id - id продукта.
    */
-
-  const setFavorite = (id) => {
+  const onToggleFavorite = (id) => {
     // Обновляем продукты на странице, переключая состояние сохраненного продукта
     const updatedProducts = products?.map((product) => {
       if (product?.id === id) {
@@ -38,28 +63,29 @@ const useProductsStore = create((set) => {
 
     // Обновляем id сохраненок для записи в localStorage
     const updatedFavorites = updatedProducts
-      ?.filter((product) => product?.isFavorite)
-      ?.map((product) => product?.id);
+      ?.filter(product => product?.isFavorite)
+      ?.map(product => product?.id);
 
     localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
 
     // Обновляем состояние.
     set({ products: updatedProducts });
-  };
+  }
 
   /**
    * Получает все сохраненные продукты.
    * @returns {Array} Массив всех сохраненных продуктов.
    */
-  const getFavoriteProducts = () =>
-    products?.filter((product) => product?.isFavorite);
+  const getFavoriteProducts = () => products?.filter(product => product?.isFavorite);
 
   return {
     products,
+    getProducts,
     getProductById,
-    setFavorite,
+    onToggleFavorite,
     getFavoriteProducts,
   };
 });
 
 export default useProductsStore;
+
